ee1ffa4ccc75cf8f033d5cc45558122e4a4693a4
RBENJAMINFRANKLIN <30905462+RBENJAMINFRANKLIN@users.noreply.github.com>
2024-09-15 20:10:16 +0530

    Time: 735 ms (15.99%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,5 @@
+/* Write your PL/SQL query statement below */
+select id,nvl(case when mod(id,2)=1 then lead(student,1) over(order by id) else
+lag(student,1) over(order by id) end,
+student)
+student from seat;
