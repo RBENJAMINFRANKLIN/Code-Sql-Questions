58472fdd373ea7ec1d1ce90c1f022b4da94fe6c4
RBENJAMINFRANKLIN <30905462+RBENJAMINFRANKLIN@users.noreply.github.com>
2025-04-23 23:51:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,8 +1,5 @@
 /* Write your PL/SQL query statement below */
-select p.product_id,
-NVL(ROUND(sum(p.price*u.units)*1.0/SUM(u.units),2),0) as average_price
-from prices p
-left join unitssold u on
-p.product_id = u.product_id
-and u.purchase_date between p.start_date and p.end_date
+select p.product_id, ROUND(sum(p.price*u.units)*1.0/sum(u.units),2) average_price from prices p  left join unitssold u on
+p.product_id = u.product_id and 
+u.purchase_date between p.start_date and p.end_date
 group by p.product_id;
