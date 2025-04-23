2b2222d697b45569d4807d67b9c3f71ce4ca9bac
RBENJAMINFRANKLIN <30905462+RBENJAMINFRANKLIN@users.noreply.github.com>
2025-04-23 23:11:23 +0530

    Time: 332 ms (89.56%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,5 @@
 /* Write your PL/SQL query statement below */
-SELECT * FROM Cinema WHERE MOD( id, 2) = 1 AND 
-
-description <> 'boring' ORDER BY rating DESC
+select id,movie, description ,rating from cinema 
+where description  not in  ('boring')
+and MOD( id, 2) = 1
+order by rating desc;
