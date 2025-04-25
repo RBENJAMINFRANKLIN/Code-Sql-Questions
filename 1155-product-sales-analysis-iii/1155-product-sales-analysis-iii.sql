eb1b957267e67ee220ddfb67f700c384fb07a72b
RBENJAMINFRANKLIN <30905462+RBENJAMINFRANKLIN@users.noreply.github.com>
2025-04-25 23:58:22 +0530

    Time: 662 ms (85.36%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,5 +1,8 @@
 /* Write your PL/SQL query statement below */
-select product_id,year as first_year,quantity,price from sales
-where (product_id,year) in (
-select product_id,min(year) from sales
-group by product_id);
+
+
+with cte as (
+    select product_id,min(year) as minyear from sales group by product_id
+)
+select  s.product_id,s.year as first_year,s.quantity,s.price from sales s inner join
+cte on cte.product_id = s.product_id and s.year = cte.minyear;
