3e3b6f42eed9c23bb6b53d7636d0077dea4b538a
RBENJAMINFRANKLIN <30905462+RBENJAMINFRANKLIN@users.noreply.github.com>
2025-04-30 22:23:58 +0530

    Time: 744 ms (47.77%), Space: 0B (100%) - LeetHub

--- 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
@@ -1,2 +1,4 @@
-/* Write your PL/SQL query statement below */
-select user_id, concat(UPPER(SUBSTR(name,1,1)),LOWER(SUBSTR(name,2,(length(name))))) as name from users order by user_id;
+# Write your MySQL query statement below
+select user_id, 
+concat(upper(substring(name,1,1)),lower(substring(name,2,length(name)))) name
+from users order by user_id;
