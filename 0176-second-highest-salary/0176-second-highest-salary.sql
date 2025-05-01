b3587f0b9dc613199e0a1b275cfa529b1a46e927
RBENJAMINFRANKLIN <30905462+RBENJAMINFRANKLIN@users.noreply.github.com>
2025-05-01 12:28:17 +0530

    Time: 446 ms (46.96%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,9 +1,2 @@
 /* Write your PL/SQL query statement below */
-WITH cte AS (
-    SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) as rank
-    FROM Employee
-)
-SELECT MAX(salary) AS SecondHighestSalary
-FROM cte
-WHERE rank = 2;
-
+select nvl(max(salary),NULL) AS SecondHighestSalary  from employee where salary not in (select max(salary) from employee);
